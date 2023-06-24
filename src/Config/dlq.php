4a3f672b7c749d2fa71e392e8995aa3e4c8f35d7
Bradley Nguyen <giang.nguyen@diag.vn>
2023-06-24 10:47:07 +0700

    Init sync all services

--- src/Config/dlq.php
+++ src/Config/dlq.php
@@ -1,6 +1,6 @@
 <?php
 return [
-    'dlq_domain' => env('DQL_DOMAIN', ''),
+    'dlq_domain' => env('DQL_DOMAIN', env('DIAG_API_URL', '')),
     'dlq_api_key' => env('DQL_API_KEY', ''),
     'dql_timeout' => env('DLQ_TIMEOUT', 3),
 ];
